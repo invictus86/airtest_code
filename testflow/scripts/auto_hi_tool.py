0a266e3c2efdfb18bcccd6a0e1d0c9698f8c5eda
82D707A6DC90BF87F91BE51A692DAAF8 <82D707A6DC90BF87F91BE51A692DAAF8@i-search.com.cn>
2019-12-06 17:13:09 +0800

    modify picture

---
+++ testflow/scripts/auto_hi_tool.py
@@ -0,0 +1,25 @@
+# -*- encoding=utf8 -*-
+__author__ = "ivan.zhao"
+
+from airtest.core.api import *
+from airtest.cli.parser import cli_setup
+
+if not cli_setup():
+    auto_setup(__file__, logdir=r"C:\Users\ivan.zhao\PycharmProjects\airtest_code\testflow\scripts\log", devices=[
+        "Windows:///",
+    ]
+               )
+
+# script content
+print("start...")
+
+assert_exists(Template(r"../res/img/hitool_hi3716mv450.png"))
+
+touch(Template(r"../res/img/jtag_network.png"))
+
+touch(Template(r"../res/img/burn.png"))
+# time.sleep(0.5)
+
+time.sleep(85)
+assert_exists(Template(r"../res/img/burn_success.png"))
+time.sleep(3)
