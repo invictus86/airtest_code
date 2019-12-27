731a6bb65131fec842ebc571a33a5782092a0db1
ivan.zhao <ivan.zhao@ekt-digital.com>
2019-12-27 18:01:09 +0800

    remove test_str

--- testflow/scripts/auto_hi_tool.py
+++ testflow/scripts/auto_hi_tool.py
@@ -6,7 +6,8 @@ from airtest.cli.parser import cli_setup
 
 if not cli_setup():
     auto_setup(__file__, logdir=r"C:\Users\ivan.zhao\PycharmProjects\airtest_code\testflow\scripts\log", devices=[
-        "Windows:///?title_re=HiTool-Hi3716MV450*",
+        # "Windows:///?title_re=HiTool-Hi3716MV450*",
+        "Windows:///",
     ]
                )
 
@@ -14,13 +15,23 @@ if not cli_setup():
 print("start...")
 
 # assert_exists(Template(r"../res/img/111.png"))
-assert_exists(Template(r"../res/img/hitoo716mv4l_hi350.png", threshold=0.9))
+# assert_exists(Template(r"../res/img/hitoo716mv4l_hi350.png", threshold=0.9))
 # Template(r"../res/img/jtag_network.png")
 
-touch(Template(r"../res/img/burn.png"))
+touch(Template(r"../res/img/burn.png", record_pos=(0.308, 0.05), resolution=(1280, 800)))
+
+# double_click(Template(r"../res/img/burn.png", record_pos=(0.308, 0.05), resolution=(1280, 800)))
+
+
+
+# double_click(Template(r"tpl1577434199857.png", rgb=True, record_pos=(-0.272, 0.187), resolution=(627, 760)))
+# touch(Template(r"tpl1577434199857.png", rgb=True, record_pos=(-0.272, 0.187), resolution=(627, 760)))
+
+# touch(Template(r"../res/img/burn.png"))
 # # time.sleep(0.5)
 #
 # time.sleep(85)
 # assert_exists(Template(r"../res/img/burn_success.png"))
 # time.sleep(3)
 # touch(
+
